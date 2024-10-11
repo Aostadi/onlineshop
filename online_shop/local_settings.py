3e5d164dba2a569640cdca1fcb208136c93bb2e8
root <root@abolfazl.openstacklocal>
2024-10-11 15:17:12 +0330

    fix conflicts

--- online_shop/local_settings.py
+++ online_shop/local_settings.py
@@ -2,8 +2,8 @@ SECRET_KEY = 'django-insecure-)$l^wly)4)!jrq5krk+2pkb=1gn9jqp0f#1g#tr5gp^f!_rv*$
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-#from pathlib import Path
-#BASE_DIR = Path(__file__).resolve().parent.parent
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 DATABASES = {
--- user/apps.py
+++ user/apps.py
@@ -1,11 +1,11 @@
 from django.apps import AppConfig
 
 
+
+
 class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user'
-<<<<<<< HEAD
-=======
+
     def ready(self):
         import user.signals
->>>>>>> 35b2c6c (upload project file)
